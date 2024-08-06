6e39cd8c5fbc092e71189ec15906078e51bb96d0
pedronovaes <pedronovaesmelo@gmail.com>
2024-08-06 00:14:40 -0300

    Add arithmetic operations and hyperbolic tangent function to the node

--- micrograd/tensor.py
+++ micrograd/tensor.py
@@ -1,18 +1,48 @@
 from typing import Union, Tuple
+import math
 
 
 class Value:
     """
     This class emulates a Tensor (from pytorch, for example) and computes
     its gradient.
+
+    Args:
+        data: int or float as a single node to be computed its gradient.
     """
     def __init__(self, data: Union[int, float], _children: Tuple = ()):
         self.data = data
-        self.grad = 0.0
+        self.grad: Value = 0.0
 
         # Internal attributes used for building autograd graph.
         self._previous = set(_children)
         self._backward = lambda: None
 
     def __repr__(self) -> str:
-        return f'Value(data={self.data})'
+        return f'Value(data={self.data}, grad={self.grad})'
+
+    def __add__(self, other):
+        output = Value(
+            data=self.data + other.data,
+            _children=(self, other)
+        )
+
+        return output
+
+    def __mul__(self, other):
+        output = Value(
+            data=self.data * other.data,
+            _children=(self, other)
+        )
+
+        return output
+
+    def tanh(self):
+        x = self.data
+        tan = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        output = Value(
+            data=tan,
+            _children=(self, )
+        )
+
+        return output
