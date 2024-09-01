38ebe7fb0da47daa4edb64977276fb3d1ca2dcab
pedronovaes <pedronovaesmelo@gmail.com>
2024-08-31 23:00:57 -0300

    Implement backward function to add, mul, and tanh operations

--- micrograd/tensor.py
+++ micrograd/tensor.py
@@ -12,7 +12,7 @@ class Value:
     """
     def __init__(self, data: Union[int, float], _children: Tuple = ()):
         self.data = data
-        self.grad: Value = 0.0
+        self.grad = 0.0
 
         # Internal attributes used for building autograd graph.
         self._previous = set(_children)
@@ -27,6 +27,14 @@ class Value:
             _children=(self, other)
         )
 
+        # Backward for add operation. Just remembering an add operation is just
+        # a distributor of gradient.
+        def _backward():
+            self.grad += output.grad
+            other.grad += output.grad
+
+        output._backward = _backward
+
         return output
 
     def __mul__(self, other):
@@ -34,6 +42,15 @@ class Value:
             data=self.data * other.data,
             _children=(self, other)
         )
+
+        # Backward for multiplication operation. Just remembering a mul
+        # operation is composed by a multiplication between the data and the
+        # gradient.
+        def _backward():
+            self.grad += other.data * output.grad
+            other.grad += self.data * output.grad
+
+        output._backward = _backward
 
         return output
 
@@ -44,5 +61,11 @@ class Value:
             data=tan,
             _children=(self, )
         )
+
+        # Backward for hyperbolic tangent operation.
+        def _backward():
+            self.grad += (1 - tan ** 2) * output.grad
+
+        output._backward = _backward
 
         return output
